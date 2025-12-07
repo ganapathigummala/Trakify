6573893e466cbce33f99c80de0062348e6d3dee7
ganapathi <ganapathigummala6712@gmail.com>
2025-12-07 17:48:00 +0530

    feat: add the module.

--- app/src/main/java/com/gana/trakify/MainScreen.kt
+++ app/src/main/java/com/gana/trakify/MainScreen.kt
@@ -9,23 +9,25 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import com.gana.trakify.navhost.NavigationHost
+import com.gana.trakify.navhost.Screen
 import com.gana.trakify.ui.theme.TrakifyTheme
 import com.gana.trakify.ui.theme.White
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    private lateinit var auth: FirebaseAuth
+    @Inject
+    lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
-        auth = Firebase.auth
 
         setContent {
             TrakifyTheme {
@@ -36,9 +38,9 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
 
                     val startDestination = if (auth.currentUser != null) {
-                        "main_screen"
+                        Screen.Main.route
                     } else {
-                        "login_screen"
+                        Screen.Login.route
                     }
 
                     NavigationHost(
